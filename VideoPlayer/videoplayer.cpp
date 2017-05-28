1346a4282be17a01620722909f0f011d39d09936
Vecnik88 <Ve_Cni_K@inbox.ru>
2017-05-29 01:32:41 +0300

    Update videoplayer.cpp

--- VideoPlayer/videoplayer.cpp
+++ VideoPlayer/videoplayer.cpp
@@ -3,8 +3,10 @@
 #include <videoplayer.h>
 
 VideoPlayer::VideoPlayer(QWidget* wgt):SoundPlayer(wgt){
+    
     QVideoWidget* pvw = new QVideoWidget;
     pvw->setMinimumSize(250,250);
     m_pvbxMainLayout->addWidget(pvw);
-    m_pmp->setVideoOutput(pvw);
+    m_pmp->setVideoOutput(pvw);   
 }
+
