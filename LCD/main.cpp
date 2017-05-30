393abf51c19e421def03e265437866e04fe76fc7
Vecnik88 <Ve_Cni_K@inbox.ru>
2017-05-30 03:01:08 +0300

    L

---
+++ LCD/main.cpp
@@ -0,0 +1,28 @@
+// электронный индикатор
+
+#include <QApplication>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QWidget wgt;
+
+    QLCDNumber* plcd = new QLCDNumber;
+    QSpinBox* pspb = new QSpinBox;
+
+    plcd->setSegmentStyle(QLCDNumber::Filled);
+    plcd->setMode(QLCDNumber::Hex);
+
+    QObject::connect(pspb, SIGNAL(valueChanged(int)), plcd, SLOT(display(int)));
+
+    QVBoxLayout* vbl = new QVBoxLayout;
+    vbl->addWidget(plcd);
+    vbl->addWidget(pspb);
+
+    wgt.setLayout(vbl);
+    wgt.resize(200,200);
+    wgt.show();
+
+    return a.exec();
+}
